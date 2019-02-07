515003c93cca6ff4beeed9bd0b3e4bc67abdc4d3
ValentinGordienko <valentingordienko.dev@yandex.ru>
2019-02-08 01:20:58 +0300

    Change pages position

--- app/src/main/java/ru/valentin_gordienko/loftmoney/MainViewPagerAdapter.java
+++ app/src/main/java/ru/valentin_gordienko/loftmoney/MainViewPagerAdapter.java
@@ -8,8 +8,8 @@ import android.support.v4.app.FragmentPagerAdapter;
 public class MainViewPagerAdapter extends FragmentPagerAdapter {
 
     private static final int PAGE_COUNT = 3;
-    private static final int PAGE_INCOME = 0;
-    private static final int PAGE_CONSUMPTION = 1;
+    private static final int PAGE_CONSUMPTION = 0;
+    private static final int PAGE_INCOME = 1;
     private static final int PAGE_BALANCE = 2;
 
     private Context context;
@@ -24,10 +24,10 @@ public class MainViewPagerAdapter extends FragmentPagerAdapter {
     public Fragment getItem(int position) {
 
         switch (position) {
-            case PAGE_INCOME:
-                return TransactionListFragment.newInstance(TransactionListFragment.TYPE_INCOME);
             case PAGE_CONSUMPTION:
                 return TransactionListFragment.newInstance(TransactionListFragment.TYPE_CONSUMPTION);
+            case PAGE_INCOME:
+                return TransactionListFragment.newInstance(TransactionListFragment.TYPE_INCOME);
             case PAGE_BALANCE:
                 return new BalanceFragment();
             default:
@@ -43,10 +43,10 @@ public class MainViewPagerAdapter extends FragmentPagerAdapter {
     public CharSequence getPageTitle(int position) {
 
         switch (position) {
-            case PAGE_INCOME:
-                return context.getString(R.string.tabCaptionIncome);
             case PAGE_CONSUMPTION:
                 return context.getString(R.string.tabCaptionConsumption);
+            case PAGE_INCOME:
+                return context.getString(R.string.tabCaptionIncome);
             case PAGE_BALANCE:
                 return context.getString(R.string.tabCaptionBalance);
             default:
