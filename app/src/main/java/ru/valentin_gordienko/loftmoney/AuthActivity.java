8a4c1f92bbf2e42365461fba524b344770fbb264
ValentinGordienko <valentingordienko.dev@yandex.ru>
2019-02-15 01:45:15 +0300

    Add "AuthActivity"

---
+++ app/src/main/java/ru/valentin_gordienko/loftmoney/AuthActivity.java
@@ -0,0 +1,14 @@
+package ru.valentin_gordienko.loftmoney;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
